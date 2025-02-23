cd447d3e1033f85f94b03344c03c2633b12c291f
Didben <didben77@gmail.com>
2025-02-23 20:36:13 +0100

    Save and Restaure functions added.

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-let h2 = document.getElementById("count_el")
+let h2 = document.getElementById("count_el");
 
 function increment() {
     let currentValue = parseInt(h2.innerText);
     if (isNaN(currentValue)) {
-        h2.innerText = 0; // default value
+        h2.innerText = 0;
     } else {
         h2.innerText = currentValue + 1;
+    }
+}
+
+function reset() {
+    h2.innerText = 0;
+}
+
+function save() {
+    localStorage.setItem('count', h2.innerText); // Save the current h2 value
+}
+
+function restaure() {
+    let storedCount = localStorage.getItem('count');
+    if (storedCount !== null) {
+        h2.innerText = storedCount;
+    } else {
+        h2.innerText = 0; // default value if nothing is stored.
     }
 }
